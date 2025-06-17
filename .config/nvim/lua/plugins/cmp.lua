4930cc2bc5e02cf8dd7d8387cdba59225cad3233
Charlie Cooksey <ccooksey@valore.com>
2025-06-17 14:51:56 +0930

    fix(lazyvim's terrible autocomplete defaults)

---
+++ .config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,7 @@
+sources = cmp.config.sources({
+  { name = "nvim_lsp" },
+  { name = "luasnip" },
+  { name = "buffer" }, -- this is what gives you words like "digital_order_items"
+  { name = "path" },
+}),
+
