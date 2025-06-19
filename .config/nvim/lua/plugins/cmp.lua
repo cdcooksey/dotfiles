8f7e215e656a1c71dfe0edfc945b6046c054eec1
Charlie Cooksey <ccooksey@valore.com>
2025-06-19 18:31:11 +0930

    fix(remove busted config file)

--- .config/nvim/lua/plugins/cmp.lua
+++
@@ -1,7 +0,0 @@
-sources = cmp.config.sources({
-  { name = "nvim_lsp" },
-  { name = "luasnip" },
-  { name = "buffer" }, -- this is what gives you words like "digital_order_items"
-  { name = "path" },
-}),
-
