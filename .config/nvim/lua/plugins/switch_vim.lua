fdb10ea65bc10c5a6f336b266af7e6ab78303f2b
Charlie Cooksey <ccooksey@valore.com>
2025-08-28 11:25:14 +0930

    feat(switch.vim)

---
+++ .config/nvim/lua/plugins/switch_vim.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "AndrewRadev/switch.vim",
+    config = function()
+      -- Optional: define custom switch definitions
+      vim.g.switch_custom_definitions = {
+        { "true", "false" },
+        { "on", "off" },
+        { "yes", "no" },
+      }
+
+      -- Map <leader>$$ to switch
+      vim.keymap.set("n", "<leader>$", ":Switch<CR>", { desc = "Switch word" })
+    end,
+  },
+}
+
