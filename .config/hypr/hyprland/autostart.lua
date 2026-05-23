f77280ebb72279971cfeb6b9528fb21c9be9c4ad
Charlie Cooksey <cdcooksey@gmail.com>
2026-05-23 23:53:22 +0930

    feat(autostart): hyprland

--- .config/hypr/hyprland/autostart.lua
+++ .config/hypr/hyprland/autostart.lua
@@ -4,8 +4,8 @@
 -- Or execute your favorite apps at launch like this:
 --
 hl.on("hyprland.start", function()
-  hl.exec_cmd("hyprpaper")
-  hl.exec_cmd("waybar")
-  hl.exec_cmd(terminal)
-  hl.exec_cmd(browser)
+	hl.exec_cmd("hyprpaper")
+	hl.exec_cmd("waybar")
+	hl.exec_cmd(browser)
+	hl.exec_cmd(terminal)
 end)
