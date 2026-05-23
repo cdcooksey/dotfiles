69b4b9e991d0c21c15c2329f64da38dd38609ecc
Charlie Cooksey <cdcooksey@gmail.com>
2026-05-23 22:07:03 +0930

    feat(.config/fish/config.fish)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -42,6 +42,12 @@ abbr pip pip3
 
 alias brew="env PATH=(string replace (pyenv root)/shims '' \"\$PATH\") brew"
 
+# Sets XDG paths
+set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_STATE_HOME $HOME/.local/state
+set -gx XDG_CACHE_HOME $HOME/.cache
+
 # Set editor
 set -gx EDITOR nvim
 
