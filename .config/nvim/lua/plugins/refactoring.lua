91fbabbccc552197dcfa8fe11302772d70eb413e
Charlie Cooksey <cdcooksey@gmail.com>
2026-06-14 02:05:06 +0930

    fix(refactoring.nvim): rip treesitter

--- .config/nvim/lua/plugins/refactoring.lua
+++ .config/nvim/lua/plugins/refactoring.lua
@@ -1,11 +1,7 @@
 return {
-  {
-    "ThePrimeagen/refactoring.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    lazy = false,
-    opts = {},
+  "ThePrimeagen/refactoring.nvim",
+  dependencies = {
+    "lewis6991/async.nvim",
   },
+  lazy = false,
 }
