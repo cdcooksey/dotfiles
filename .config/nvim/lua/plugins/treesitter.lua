3c351a8ba35e58e28d4431fb6ff23eaeb3e5c863
Charlie Cooksey <cdcooksey@gmail.com>
2026-06-14 15:00:59 +0930

    fix(nvim): clean up

--- .config/nvim/lua/plugins/treesitter.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    highlight = {
-      enable = true,
-      disable = { "ruby" }, -- languages you don’t want treesitter highlighting for
-    },
-  },
-}
