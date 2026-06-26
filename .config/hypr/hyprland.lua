9e1bea59540e4c60981bc851eb43862254a678b5
Charlie Cooksey <cdcooksey@gmail.com>
2026-06-27 05:38:10 +0930

    feat(hyprland): conf

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -13,24 +13,24 @@
 ---- MONITORS ----
 ------------------
 
-require('hyprland.monitor')
+require("hyprland.monitor")
 
 ---------------------
 ---- MY PROGRAMS ----
 ---------------------
 
-require('hyprland.programs')
+require("hyprland.programs")
 -------------------
 ---- AUTOSTART ----
 -------------------
 
-require('hyprland.autostart')
+require("hyprland.autostart")
 
 -------------------------------
 ---- ENVIRONMENT VARIABLES ----
 -------------------------------
 
-require('hyprland.env_vars')
+require("hyprland.env_vars")
 
 -----------------------
 ----- PERMISSIONS -----
@@ -54,9 +54,9 @@ require('hyprland.env_vars')
 ---- LOOK AND FEEL ----
 -----------------------
 
-require('hyprland.variables')
+require("hyprland.variables")
 
-require('hyprland.animation')
+require("hyprland.animation")
 
 -- Ref https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
 -- "Smart gaps" / "No gaps when only"
@@ -78,23 +78,23 @@ require('hyprland.animation')
 
 -- See https://wiki.hypr.land/Configuring/Layouts/Dwindle-Layout/ for more
 hl.config({
-  dwindle = {
-    preserve_split = true, -- You probably want this
-  },
+	dwindle = {
+		preserve_split = true, -- You probably want this
+	},
 })
 
 -- See https://wiki.hypr.land/Configuring/Layouts/Master-Layout/ for more
 hl.config({
-  master = {
-    new_status = "master",
-  },
+	master = {
+		new_status = "master",
+	},
 })
 
 -- See https://wiki.hypr.land/Configuring/Layouts/Scrolling-Layout/ for more
 hl.config({
-  scrolling = {
-    fullscreen_on_one_column = true,
-  },
+	scrolling = {
+		fullscreen_on_one_column = true,
+	},
 })
 
 ----------------
@@ -102,10 +102,10 @@ hl.config({
 ----------------
 
 hl.config({
-  misc = {
-    force_default_wallpaper = -1,  -- Set to 0 or 1 to disable the anime mascot wallpapers
-    disable_hyprland_logo = false, -- If true disables the random hyprland logo / anime girl background. :(
-  },
+	misc = {
+		force_default_wallpaper = -1, -- Set to 0 or 1 to disable the anime mascot wallpapers
+		disable_hyprland_logo = false, -- If true disables the random hyprland logo / anime girl background. :(
+	},
 })
 
 ---------------
@@ -113,41 +113,41 @@ hl.config({
 ---------------
 
 hl.config({
-  input = {
-    kb_layout = "us",
-    kb_variant = "",
-    kb_model = "",
-    kb_options = "",
-    kb_rules = "",
+	input = {
+		kb_layout = "us",
+		kb_variant = "",
+		kb_model = "",
+		kb_options = "",
+		kb_rules = "",
 
-    follow_mouse = 1,
+		follow_mouse = 1,
 
-    sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+		sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
 
-    touchpad = {
-      natural_scroll = false,
-    },
-  },
+		touchpad = {
+			natural_scroll = false,
+		},
+	},
 })
 
 hl.gesture({
-  fingers = 3,
-  direction = "horizontal",
-  action = "workspace",
+	fingers = 3,
+	direction = "horizontal",
+	action = "workspace",
 })
 
 -- Example per-device config
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Devices/ for more
 hl.device({
-  name = "epic-mouse-v1",
-  sensitivity = -0.5,
+	name = "epic-mouse-v1",
+	sensitivity = -0.5,
 })
 
 ---------------------
 ---- KEYBINDINGS ----
 ---------------------
 
-require('hyprland.keybinds')
+require("hyprland.keybinds")
 
 --------------------------------
 ---- WINDOWS AND WORKSPACES ----
@@ -159,27 +159,27 @@ require('hyprland.keybinds')
 -- Example window rules that are useful
 
 local suppressMaximizeRule = hl.window_rule({
-  -- Ignore maximize requests from all apps. You'll probably like this.
-  name = "suppress-maximize-events",
-  match = { class = ".*" },
+	-- Ignore maximize requests from all apps. You'll probably like this.
+	name = "suppress-maximize-events",
+	match = { class = ".*" },
 
-  suppress_event = "maximize",
+	suppress_event = "maximize",
 })
 -- suppressMaximizeRule:set_enabled(false)
 
 hl.window_rule({
-  -- Fix some dragging issues with XWayland
-  name = "fix-xwayland-drags",
-  match = {
-    class = "^$",
-    title = "^$",
-    xwayland = true,
-    float = true,
-    fullscreen = false,
-    pin = false,
-  },
+	-- Fix some dragging issues with XWayland
+	name = "fix-xwayland-drags",
+	match = {
+		class = "^$",
+		title = "^$",
+		xwayland = true,
+		float = true,
+		fullscreen = false,
+		pin = false,
+	},
 
-  no_focus = true,
+	no_focus = true,
 })
 
 -- Layer rules also return a handle.
@@ -192,9 +192,9 @@ hl.window_rule({
 
 -- Hyprland-run windowrule
 hl.window_rule({
-  name = "move-hyprland-run",
-  match = { class = "hyprland-run" },
+	name = "move-hyprland-run",
+	match = { class = "hyprland-run" },
 
-  move = "20 monitor_h-120",
-  float = true,
+	move = "20 monitor_h-120",
+	float = true,
 })
--- .config/hypr/hyprland/variables.lua
+++ .config/hypr/hyprland/variables.lua
@@ -1,49 +1,62 @@
 -- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 hl.config({
-  general = {
-    gaps_in = 5,
-    gaps_out = 15,
+	general = {
+		gaps_in = 15,
+		gaps_out = 15,
 
-    border_size = 2,
+		border_size = 2,
 
-    col = {
-      active_border = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
-      inactive_border = "rgba(595959aa)",
-    },
+		col = {
+			active_border = {
+				colors = {
+					"#232634",
+					"#949cbb",
+				},
+				angle = 45,
+			},
+			-- inactive_border = "rgba(595959aa)",
+			inactive_border = "#232634",
+		},
 
-    -- Set to true to enable resizing windows by clicking and dragging on borders and gaps
-    resize_on_border = true,
+		-- Set to true to enable resizing windows by clicking and dragging on borders and gaps
+		resize_on_border = true,
 
-    -- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
-    allow_tearing = false,
+		-- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
+		allow_tearing = false,
 
-    layout = "dwindle",
-  },
+		layout = "dwindle",
+	},
 
-  decoration = {
-    rounding = 10,
-    rounding_power = 2,
+	decoration = {
+		rounding = 8,
+		rounding_power = 3,
 
-    -- Change transparency of focused and unfocused windows
-    active_opacity = 1.0,
-    inactive_opacity = 1.0,
+		-- Change transparency of focused and unfocused windows
+		active_opacity = 1.0,
+		inactive_opacity = 0.85,
 
-    shadow = {
-      enabled = true,
-      range = 4,
-      render_power = 3,
-      color = 0xee1a1a1a,
-    },
+		shadow = {
+			enabled = false,
+			range = 4,
+			render_power = 3,
+			color = "#303446",
+		},
 
-    blur = {
-      enabled = true,
-      size = 3,
-      passes = 1,
-      vibrancy = 0.1696,
-    },
-  },
+		blur = {
+			enabled = true,
+			size = 3,
+			passes = 1,
+			vibrancy = 0.1696,
+		},
 
-  animations = {
-    enabled = true,
-  },
+		glow = {
+			enabled = false,
+			range = 15,
+			render_power = 4,
+		},
+	},
+
+	animations = {
+		enabled = true,
+	},
 })
