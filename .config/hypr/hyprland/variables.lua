ec4f1990e70672414f06d4aa891806bb14dac7af
Charlie Cooksey <cdcooksey@gmail.com>
2026-06-27 19:30:03 +0930

    feat(inactive_opacity): 0.75

--- .config/hypr/hyprland/variables.lua
+++ .config/hypr/hyprland/variables.lua
@@ -32,7 +32,7 @@ hl.config({
 
     -- Change transparency of focused and unfocused windows
     active_opacity = 1.0,
-    inactive_opacity = 0.85,
+    inactive_opacity = 0.75,
 
     shadow = {
       enabled = false,
