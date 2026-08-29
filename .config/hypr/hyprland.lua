982a3511bb8f367ade8b0b4a68f6e5a1b2800487
Charlie Cooksey <cdcooksey@gmail.com>
2026-08-29 18:10:26 +0930

    fix(hyprland): disable_autoreload

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -106,6 +106,7 @@ hl.config({
 		force_default_wallpaper = -1, -- Set to 0 or 1 to disable the anime mascot wallpapers
 		disable_hyprland_logo = false, -- If true disables the random hyprland logo / anime girl background. :(
     disable_splash_rendering = true
+    disable_autoreload = true
 	},
 })
 
